1ba608af2136442e7bce114c04797a723719ca62
Mark THEBAULT <mthebault@ippon.fr>
2017-06-13 15:16:03 +0200

    tests with consul

---
+++ start-swarm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source variable.cfg
+
+
+
+for i in $(seq 1 $NODES); do
+    docker-machine create -d virtualbox --engine-opt experimental swarm-$i
+done
+
+eval $(docker-machine env swarm-1)
+
+docker swarm init --advertise-addr $(docker-machine ip swarm-1)
+
+TOKEN=$(docker swarm join-token -q manager)
+
+for i in $(seq 2 $NODES); do
+  eval $(docker-machine env swarm-$i)
+  docker swarm join --token $TOKEN --advertise-addr $(docker-machine ip swarm-$i) $(docker-machine ip swarm-1):2377
+done
